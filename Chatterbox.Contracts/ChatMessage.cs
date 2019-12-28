c7f34e1d492bce3b9c59b4729df1f8ec6e8e92cd
doklem <d.klemm@mt-robot.com>
2019-12-28 12:16:02 +0100

    Defined the contracts between client and server

---
+++ Chatterbox.Contracts/ChatMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chatterbox.Contracts
+{
+    /// <summary>
+    /// Represents a message of the chat.
+    /// </summary>
+    public class ChatMessage
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the message.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message's sender.
+        /// </summary>
+        public string Sender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message's text.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message's reception time (UTC) of the server.
+        /// </summary>
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Chatterbox.Contracts/IChat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Chatterbox.Contracts
+{
+    /// <summary>
+    /// Represents the API of the chat SignalR hub.
+    /// </summary>
+    public interface IChat
+    {
+        /// <summary>
+        /// Requests the history of <see cref="ChatMessage"/>s.
+        /// </summary>
+        Task GetHistoryAsync();
+
+        /// <summary>
+        /// Returns the history of <see cref="ChatMessage"/> to the caller of the <see cref="GetHistoryAsync"/>-method.
+        /// </summary>
+        /// <param name="history">This <see cref="IEnumerable{T}"/> contains all <see cref="ChatMessage"/>, which were sent by the server until now.</param>
+        Task ObtainHistoryAsync(IEnumerable<ChatMessage> history);
+
+        /// <summary>
+        /// Each client of the chat will obtain new <see cref="ChatMessage"/> through this method.
+        /// </summary>
+        /// <param name="message">This <see cref="ChatMessage"/> represents the new message, which was sent by a member of the chat.</param>
+        Task ObtainMessageAsync(ChatMessage message);
+
+        /// <summary>
+        /// The clients of the chat can send new <see cref="ChatMessage"/> through this method.
+        /// </summary>
+        /// <param name="message">This <see cref="string"/> will become the <see cref="ChatMessage"/>'s <see cref="ChatMessage.Text"/>.</param>
+        /// <param name="sender">This <see cref="string"/> will become the <see cref="ChatMessage"/>'s <see cref="ChatMessage.Sender"/>.</param>
+        Task SendMessageAsync(string message, string sender);
+    }
+}
