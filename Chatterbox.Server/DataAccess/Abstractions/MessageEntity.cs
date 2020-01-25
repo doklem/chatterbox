f09ac67ebfc71e464c7c08cfb2dbf3d145c7927b
doklem <dominicklemm@gmx.ch>
2020-01-25 17:47:07 +0100

    Validate with dependency diagram

--- Chatterbox.Server/DataAccess/Abstractions/MessageEntity.cs
+++ Chatterbox.Server/DataAccess/Abstractions/MessageEntity.cs
@@ -8,28 +8,28 @@ namespace Chatterbox.Server.DataAccess.Abstractions
     /// </summary>
     public class MessageEntity
     {
-        [Key]
         /// <summary>
         /// Gets or sets the unique identifier of the message.
         /// </summary>
+        [Key]
         public long Id { get; set; }
 
-        [Required]
         /// <summary>
         /// Gets or sets the message's sender.
         /// </summary>
+        [Required]
         public string Sender { get; set; }
 
-        [Required]
         /// <summary>
         /// Gets or sets the message's text.
         /// </summary>
+        [Required]
         public string Text { get; set; }
 
-        [Required]
         /// <summary>
         /// Gets or sets the message's reception time (UTC) of the server.
         /// </summary>
+        [Required]
         public DateTime Time { get; set; }
     }
 }
